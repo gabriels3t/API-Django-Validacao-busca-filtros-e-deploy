f39167feb530b3369677fd0e860578e6e4488f30
gabriels3t <gabriels3t@hotmail.com>
2021-01-25 13:51:24 -0300

    Importando validações e gerando clientes

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -8,14 +8,14 @@ class ClienteSerializer(serializers.ModelSerializer):
     
     def validate(self, data): 
         if  not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf':'O CPF deve ter 11 Dígitos'})              
+            raise serializers.ValidationError({'cpf':'Número de CPF inválido'})              
         if not Verificando_se_eh_letra(data['nome']):
             raise serializers.ValidationError({'nome':'Não inclua números nesse campo'})
         
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg':'O RG deve ter 9 dígitos'})
         
-        if celular_valido(data['celular']):
-             raise serializers.ValidationError({'celular':'O celular deve ter pelo menos 11 dígitos '})
+        if not celular_valido(data['celular']):
+             raise serializers.ValidationError({'celular':'O número de celular deve seguir este modelo: 15 91234-1234'})
         
         return data
--- clientes/validators.py
+++ clientes/validators.py
@@ -1,5 +1,10 @@
+import re 
+from validate_docbr import CPF
+
 def cpf_valido(numero_do_cpf):
- return len(numero_do_cpf) == 11
+    cpf = CPF()
+    return cpf.validate(numero_do_cpf)
+    
 
 def Verificando_se_eh_letra(nome):
     return  nome.isalpha()
@@ -9,6 +14,12 @@ def rg_valido(rg):
     return len(rg) == 9
 
 def celular_valido(celular):
-    return len(celular) < 11
+    """
+    Verifica se o celular é valido (11 91234-1234)
+    """
+    modelo ='[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo,celular)
+    return resposta
+    
        
     
